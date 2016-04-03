ee328bbd4e89f4c97f426064b1ddf3d5a0111740
pinusc <giuseppe.stelluto@gmail.com>
2016-04-03 15:11:50 +0200

    Add scroll effect for sheet on page #4

---
+++ js/scroll_move.js
@@ -0,0 +1,20 @@
+function map(x, in_min, in_max, out_min, out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+function main() {
+    var doc = document.documentElement;
+    var height = window.innerHeight;
+    window.onscroll = function() {
+        var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+        // console.log(top);
+        if (top >= height * 2 && top <= height * 3) {
+            var pos = top - height * 2;
+            var toBe = map(pos, 0, height, 100, 15);
+            document.getElementById("sheet").style.marginTop = "" + toBe + "vh";
+        }
+    };
+
+}
+window.onload = main;
