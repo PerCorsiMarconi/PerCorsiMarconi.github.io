99f118e50f398136de3f4cd80ef1f06b09468c97
pinusc <giuseppe.stelluto@gmail.com>
2016-04-03 18:15:08 +0200

    Add rotate animation for planet image

--- js/scroll_move.js
+++ js/scroll_move.js
@@ -8,11 +8,17 @@ function main() {
     var height = window.innerHeight;
     window.onscroll = function() {
         var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-        // console.log(top);
+        // sheet at page 3
         if (top >= height * 2 && top <= height * 3) {
             var pos = top - height * 2;
             var toBe = map(pos, 0, height, 100, 20);
             document.getElementById("sheet").style.marginTop = "" + toBe + "vh";
+        } else if (top >= height * 0 && top <= height * 1) {
+            console.log("l");
+            var pos = top;
+            var toBe = map(pos, 0, height, 0, 360);
+            console.log(toBe);
+            document.getElementById("planet").style.transform = "rotate(" + toBe + "deg)";
         }
     };
 
